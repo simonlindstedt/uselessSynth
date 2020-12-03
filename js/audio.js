e6dab6eb8f0194eadd5578ed767eab2b4313ac86
simonlindstedt <70521702+simonlindstedt@users.noreply.github.com>
2020-12-03 11:32:23 +0100

    added filter, lfo and vibrato. Plus, mouse!

--- js/audio.js
+++ js/audio.js
@@ -2,27 +2,56 @@ const startButton = document.querySelector("button[id='start']");
 const stopButton = document.querySelector("button[id='stop']");
 const volume = document.querySelector("input[id='volume']");
 const freq = document.querySelector("input[id='freq']");
+let h = window.innerHeight;
+let w = window.innerWidth;
 
 const audioCtx = new AudioContext();
 audioCtx.suspend();
+
 const masterVolume = audioCtx.createGain();
-const oscs = [];
-const numberOfOscs = 16;
-
-for (let i = 0; i < numberOfOscs; i++) {
-  let osc = audioCtx.createOscillator();
-  let oscGain = audioCtx.createGain();
-  oscGain.gain.value = 0.5 / numberOfOscs;
-  osc.connect(oscGain);
-  oscGain.connect(masterVolume);
-  oscs[i] = [osc, oscGain];
-}
-
 masterVolume.connect(audioCtx.destination);
 masterVolume.gain.value = volume.value;
 
+const filter = audioCtx.createBiquadFilter();
+filter.type = "lowpass";
+filter.connect(masterVolume);
+
+const vibrato = audioCtx.createGain();
+vibrato.gain.value = 50;
+
+const lfo = audioCtx.createOscillator();
+lfo.connect(vibrato);
+lfo.frequency.value = 5;
+
+const oscs = [];
+const numberOfOscs = 8;
+
+for (let i = 0; i < numberOfOscs; i++) {
+  let random = Math.random();
+  let osc = audioCtx.createOscillator();
+  let oscGain = audioCtx.createGain();
+  osc.type = "sawtooth";
+  vibrato.connect(osc.detune);
+  oscGain.gain.value = 0.5 / numberOfOscs;
+  osc.detune.value = Math.floor(random * 100) + 1;
+  osc.connect(oscGain);
+  oscGain.connect(filter);
+  oscs[i] = [osc, oscGain];
+}
+
 startOscillators(oscs);
 setFrequency(oscs, freq.value);
+
+// test
+
+window.addEventListener("resize", () => {
+  w = window.innerHeight;
+  h = window.innerWidth;
+});
+
+document.body.addEventListener("mousemove", (e) => {
+  filter.frequency.value = setFilter(10000, e.x);
+});
 
 startButton.addEventListener("click", () => {
   audioCtx.resume();
@@ -41,6 +70,8 @@ freq.addEventListener("change", () => {
 });
 
 function startOscillators(oscs) {
+  lfo.start(0);
+
   oscs.forEach((oscItem) => {
     oscItem[0].start(0);
   });
@@ -52,16 +83,13 @@ function setFrequency(oscs, freq) {
 
   for (let i = 0; i < oscs.length; i++) {
     if (i === 0) {
-      console.log("baseFreq:", baseFreq);
       oscs[i][0].frequency.value = baseFreq;
     } else {
       if (semitone === 0) {
         semitone = 3;
-        console.log(intervalToFrequency(baseFreq, semitone));
         oscs[i][0].frequency.value = intervalToFrequency(baseFreq, semitone);
       } else {
         semitone += 3;
-        console.log(intervalToFrequency(baseFreq, semitone));
         oscs[i][0].frequency.value = intervalToFrequency(baseFreq, semitone);
       }
     }
@@ -70,4 +98,9 @@ function setFrequency(oscs, freq) {
 
 function intervalToFrequency(baseFrequency, semitone) {
   return baseFrequency * Math.pow(2, semitone / 12);
+}
+
+function setFilter(maxFreq, xPosition) {
+  let incr = maxFreq / w;
+  return xPosition * incr;
 }
